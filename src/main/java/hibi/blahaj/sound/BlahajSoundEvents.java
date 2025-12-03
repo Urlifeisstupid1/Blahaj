3e4f7a4d2c47e12a9ab3b6dc9a923ac0228cfac7
Urlifeisstupid <coburnstaniland30107@gmail.com>
2025-12-03 19:19:10 +0000

    Update BlahajSoundEvents.java

--- src/main/java/hibi/blahaj/sound/BlahajSoundEvents.java
+++ src/main/java/hibi/blahaj/sound/BlahajSoundEvents.java
@@ -1,27 +1,33 @@
 package hibi.blahaj.sound;
 
-import hibi.blahaj.*;
-import net.minecraft.registry.*;
-import net.minecraft.sound.*;
-import net.minecraft.util.*;
+import hibi.blahaj.Blahaj;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.random.Random;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BlahajSoundEvents {
 
-    public static final List<SoundEvent> BLOCK_CUDDLY_ITEM = new ArrayList<>();
-    public static final SoundEvent BLOCK_CUDDLY_ITEM_HIT = register("block.blahaj.cuddly_item.hit");
+	public static final List<SoundEvent> BLOCK_CUDDLY_ITEM = new ArrayList<>();
 
-    private static Identifier id(String id) {
-        return Identifier.of(Blahaj.MOD_ID, id);
-    }
+	public static final SoundEvent BLOCK_CUDDLY_ITEM_HIT =
+		register("block.blahaj.cuddly_item.hit");
 
-    private static SoundEvent register(String id) {
-        return Registry.register(Registries.SOUND_EVENT, id(id), SoundEvent.of(id(id)));
-    }
+	private static Identifier id(String path) {
+		return Identifier.of(Blahaj.MOD_ID, path);
+	}
 
-    public static void init() {
+	private static SoundEvent register(String path) {
+		Identifier identifier = id(path);
+		SoundEvent event = SoundEvent.of(identifier);
+		return Registry.register(Registries.SOUND_EVENT, identifier, event);
+	}
+
+	public static void init() {
 		for (int i = 1; i < 6; i++) {
 			BLOCK_CUDDLY_ITEM.add(register("block.blahaj.cuddly_item.use." + i));
 		}
@@ -30,5 +36,4 @@ public class BlahajSoundEvents {
 	public static SoundEvent getRandomSqueak(Random random) {
 		return BLOCK_CUDDLY_ITEM.get(random.nextInt(BLOCK_CUDDLY_ITEM.size()));
 	}
-
 }
