249ee3f9e78873c0bdbd14c267ed0dc51d68b889
Urlifeisstupid <coburnstaniland30107@gmail.com>
2025-12-03 19:21:31 +0000

    Update BlahajBlocks.java

--- src/main/java/hibi/blahaj/block/BlahajBlocks.java
+++ src/main/java/hibi/blahaj/block/BlahajBlocks.java
@@ -1,14 +1,13 @@
 package hibi.blahaj.block;
 
-import net.fabricmc.fabric.api.client.rendering.v1.*;
-import net.fabricmc.fabric.api.itemgroup.v1.*;
 import net.minecraft.block.*;
-import net.minecraft.client.render.*;
 import net.minecraft.entity.*;
 import net.minecraft.item.*;
+import net.minecraft.text.Text;
+import eu.pb4.polymer.core.api.item.*;
+import hibi.blahaj.Blahaj;
 import net.minecraft.registry.*;
 import net.minecraft.util.*;
-
 import java.util.*;
 
 import static hibi.blahaj.Blahaj.*;
@@ -28,53 +27,67 @@ public class BlahajBlocks {
 	public static Block BROWN_BEAR_BLOCK;
 
 	public static final List<String> PRIDE_NAMES = List.of(
-		"ace", "agender", "aro", "aroace", "bi", "demiboy", "demigirl",
-		"demi_r", "demi_s", "enby", "gay", "genderfluid", "genderqueer", "greyrose",
-		"grey_r", "grey_s", "intersex", "lesbian", "pan", "poly", "pride", "trans");
+			"ace", "agender", "aro", "aroace", "bi", "demiboy", "demigirl",
+			"demi_r", "demi_s", "enby", "gay", "genderfluid", "genderqueer", "greyrose",
+			"grey_r", "grey_s", "intersex", "lesbian", "pan", "poly", "pride", "trans");
 
 	public static List<Block> BLOCKS = new ArrayList<>();
 	public static List<Item> ITEMS = new ArrayList<>();
 
+	public static final ItemGroup ITEM_GROUP = PolymerItemGroupUtils.builder()
+			.displayName(Text.of("Blåhaj"))
+			.icon(() -> new ItemStack(BLAHAJ_BLOCK)).entries((context, entries) -> {
+				for (Item item : ITEMS) {
+					entries.add(new ItemStack(item));
+				}
+			}).build();
+
 	public static void register() {
+
 		GRAY_SHARK_BLOCK = registerCuddlyBlockAndItem(GRAY_SHARK_ID, "block.blahaj.gray_shark.tooltip");
 		BLAHAJ_BLOCK = registerCuddlyBlockAndItem(BLAHAJ_ID, "block.blahaj.blue_shark.tooltip");
 		BLAVINGAD_BLOCK = registerCuddlyBlockAndItem(BLAVINGAD_ID, "block.blahaj.blue_whale.tooltip");
-		BREAD_BLOCK = registerCuddlyBlockAndItem(BREAD_ID, null);
+		BREAD_BLOCK = registerCuddlyBlockAndItem(BREAD_ID, "block.blahaj.bread.tooltip");
 		BROWN_BEAR_BLOCK = registerCuddlyBlockAndItem(BROWN_BEAR_ID, "block.blahaj.brown_bear.tooltip");
 
 		for (String name : PRIDE_NAMES) {
 			Identifier id = Identifier.of(MOD_ID, name + "_shark");
 			registerCuddlyBlockAndItem(id, "block.blahaj.blue_shark.tooltip");
 		}
+		PolymerItemGroupUtils.registerPolymerItemGroup(Identifier.of(MOD_ID, "item_group"), ITEM_GROUP);
 
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
-			for (Item item : ITEMS) {
-				entries.add(new ItemStack(item));
-			}
-		});
 	}
 
 	public static Block registerCuddlyBlockAndItem(Identifier id, String tooltip) {
+
 		RegistryKey<Block> blockKey = RegistryKey.of(RegistryKeys.BLOCK, id);
 		RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, id);
-		Block block = Registry.register(Registries.BLOCK, id, new CuddlyBlock(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL).registryKey(blockKey)));
-		Item item = Registry.register(Registries.ITEM, id, new CuddlyItem(block, new Item.Settings()
-			.registryKey(itemKey)
-			.useBlockPrefixedTranslationKey()
-			.maxCount(1)
-			.attributeModifiers(CuddlyItem.createAttributeModifiers())
-			.equippableUnswappable(EquipmentSlot.HEAD), tooltip));
 
-		BLOCKS.add(block);
+		// Note the type: CuddlyBlock, which implements PolymerBlock
+		CuddlyBlock block = Registry.register(
+			Registries.BLOCK,
+			id,
+			new CuddlyBlock(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL).registryKey(blockKey))
+		);
+
+		Item item = Registry.register(
+			Registries.ITEM,
+			id,
+			new CuddlyItem(
+				block,
+				new Item.Settings()
+					.registryKey(itemKey)
+					.useBlockPrefixedTranslationKey()
+					.maxCount(1)
+					.attributeModifiers(CuddlyItem.createAttributeModifiers())
+					.equipmentSlot((entity, stack) -> EquipmentSlot.HEAD),
+				tooltip
+			)
+		);
+
 		ITEMS.add(item);
-
+		BLOCKS.add(block);
 		return block;
-	}
-
-	public static void registerClient() {
-		for (Block block : BLOCKS) {
-			BlockRenderLayerMap.putBlock(block, BlockRenderLayer.CUTOUT);
-		}
 	}
 
 }
