77e313823d984d24af05a911654c5c8fb0d4167f
Urlifeisstupid <coburnstaniland30107@gmail.com>
2025-12-03 19:22:41 +0000

    Create CuddlyBlockEntity.java

---
+++ src/main/java/hibi/blahaj/block/CuddlyBlockEntity.java
@@ -0,0 +1,18 @@
+package hibi.blahaj.block;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.util.math.BlockPos;
+import eu.pb4.polymer.core.api.block.PolymerBlockUtils;
+import hibi.blahaj.block.*;
+/*
+public class CuddlyBlockEntity extends BlockEntity {
+
+    public CuddlyBlockEntity(BlockPos pos, BlockState state) {
+        //super(BlahajBlocks.BLAHAJ_BLOCK, pos, state);
+        //PolymerBlockUtils.registerBlockEntity();
+    }
+
+}
+*/
