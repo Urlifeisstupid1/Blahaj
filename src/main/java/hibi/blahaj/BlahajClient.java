0273b26e0ad31810595bba4f5a1b86f4a4c3cebe
Urlifeisstupid <coburnstaniland30107@gmail.com>
2025-12-03 19:20:44 +0000

    Update BlahajClient.java

--- src/main/java/hibi/blahaj/BlahajClient.java
+++ src/main/java/hibi/blahaj/BlahajClient.java
@@ -1,13 +1,1 @@
-package hibi.blahaj;
 
-import hibi.blahaj.block.*;
-import net.fabricmc.api.*;
-
-public class BlahajClient implements ClientModInitializer {
-
-	@Override
-	public void onInitializeClient() {
-		BlahajBlocks.registerClient();
-	}
-
-}
